54c44f513e4a99fa85fac446f3e347a8c5e28488
Sonia Dumitru <soni.dumitru@gmail.com>
2019-11-22 12:41:42 -0600

    search react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+constructor() {
+  super()
+  this.state = {
+    names: ['sonia', 'diana', 'lili'],
+    searchName: ''
+  }
+}
+
+handleFilter(event) {
+  this.setState({
+    searchName: event.target.value
+  })
+}
+
+render() {
+    return (
+      <div>
+      <SearchBar handleFilter={(event) => this.handleFilter(event)}/>
+      <ul>{this.state.names
+        .filter(name => name.toLowerCase().
+        includes(this.state.searchName.toLowerCase())).map(name => <li>{name}</li>)}</ul>
+      </div>
+    );
+  }
+}
+
+
+function SearchBar(props) {
+  return(
+    <input type="text" onChange={props.handleFilter}></input>
+  )
 }
 
 export default App;
